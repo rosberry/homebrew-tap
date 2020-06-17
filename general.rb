53f9c136157f10ccd2501c822dd39bcd91ad1f2d
Artem Novichkov <artem.novichkov@rosberry.com>
2020-06-17 11:05:53 +0600

    Update sha

--- general.rb
+++ general.rb
@@ -3,7 +3,7 @@ class General < Formula
     homepage "https://github.com/rosberry/general"
     url "https://github.com/rosberry/general/archive/0.1.0.tar.gz"
     version "0.1.0"
-    sha256 ""
+    sha256 "ade9133eaa212d2b9cfbb3b45e0d2fcc24d6af27311a0da0bb14099048ece88e"
 
     def install
         system "swift", "build",
