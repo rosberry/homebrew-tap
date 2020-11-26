d3a2e06d2abed4bdf9a19868cc2180916e94834a
Nik <nikolay.tyunin@rosberry.com>
2020-11-26 14:15:51 +0600

    Update general.rb

--- general.rb
+++ general.rb
@@ -1,8 +1,8 @@
 class General < Formula
     desc "Code generation for your modules"
     homepage "https://github.com/rosberry/general"
-    url "https://github.com/rosberry/general/archive/0.1.1.tar.gz"
-    version "0.1.1"
+    url "https://github.com/rosberry/general/archive/0.1.2.tar.gz"
+    version "0.1.2"
     sha256 "a5c9d460b1fbb9ad16efca2c1c76f1a7a13195500e2ff8d85f7fa5c0ab837349"
 
     def install
