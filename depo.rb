bb81e661e383e105af85f752cede003726940720
zhvrnkov <vlad.zhavoronkov@rosberry.com>
2020-12-04 09:19:41 +0600

    Bump depo

--- depo.rb
+++ depo.rb
@@ -1,8 +1,8 @@
 class Depo < Formula
     desc "Tool for convenient multiple package managers usage in your iOS project"
     homepage "https://github.com/rosberry/depo"
-    url "https://github.com/rosberry/depo/archive/1.0.1.tar.gz"
-    version "1.0.1"
+    url "https://github.com/rosberry/depo/archive/1.0.2.tar.gz"
+    version "1.0.2"
     sha256 "ea5512746444fb050e100fc78a639190bad10786d545f7071ca73c514dc18655"
   
     def install
