832b588579ff6e373b66af9c5cfa488df0871ade
zhvrnkov <vlad.zhavoronkov@rosberry.com>
2020-12-04 09:25:41 +0600

    Fix bump.sh
    sha256 calculations was wrong since this script first update
    sha (which is calculated from old version) and then update
    version

--- bump.sh
+++ bump.sh
@@ -6,8 +6,8 @@ PROJ=$1
 VERSION=$2
 FORMULA=$PROJ'.rb'
 
-SHA256=$(shasum --algorithm 256 $(brew --cache --build-from-source $FORMULA) | awk '{print $1}')
-
-sed -E -i '' 's/^    sha256 ".+"/    sha256 "'$SHA256\"/ $FORMULA
 sed -E -i '' 's/^    version ".+"/    version "'$VERSION\"/ $FORMULA
 sed -E -i '' 's/^    url ".+"/    url "https:\/\/github.com\/rosberry\/'$PROJ'\/archive\/'$VERSION'.tar.gz'\"/ $FORMULA
+
+SHA256=$(shasum --algorithm 256 $(brew --cache --build-from-source $FORMULA) | awk '{print $1}')
+sed -E -i '' 's/^    sha256 ".+"/    sha256 "'$SHA256\"/ $FORMULA
