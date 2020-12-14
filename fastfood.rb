3636a56dcf8d1ee51a228375d28fe9f3f39fae30
ntunin <nik.tyunin.92@mail.ru>
2020-12-14 14:26:04 +0600

    Update fastfood.rb

--- fastfood.rb
+++ fastfood.rb
@@ -2,7 +2,7 @@ class Fastfood < Formula
   desc "🍔🍟 Share Fastfile between projects"
   homepage "https://github.com/rosberry/fastfood"
   version "1.4.3"
-  url "https://github.com/rosberry/fastfood/releases/download/#{version}/fastfood.zip"
+  url "https://github.com/rosberry/fastfood/archive/#{version}/fastfood.zip"
   sha256 "799c7fd057ef97b4ff258cec420eae4f70bbd225bcc94585c3d980355facc197"
 
   depends_on :xcode => ["10.2", :build]
