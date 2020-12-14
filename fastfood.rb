799b503bdf6dc139054d79b02bb905777dbbddee
ntunin <nik.tyunin.92@mail.ru>
2020-12-14 14:49:27 +0600

    remove sha256

--- fastfood.rb
+++ fastfood.rb
@@ -3,7 +3,6 @@ class Fastfood < Formula
   homepage "https://github.com/rosberry/fastfood"
   version "1.4.3"
   url "https://github.com/rosberry/fastfood/archive/#{version}/fastfood.zip"
-  sha256 "28574e2e060a7cc95f1d58e25c52eaf83ed8ea6a132aae4676c2277fb5023578"
 
   depends_on :xcode => ["10.2", :build]
 
