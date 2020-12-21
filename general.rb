3233a53089742c465bea2d9e83cd164b1e23876c
zhvrnkov <vlad.zhavoronkov@rosberry.com>
2020-12-21 15:50:01 +0600

    Complete general formula

--- general.rb
+++ general.rb
@@ -1,13 +1,16 @@
 class General < Formula
     desc "Code generation for your modules"
     homepage "https://github.com/rosberry/general"
-    url "https://github.com/rosberry/general/tarball/brew"
+    url "https://github.com/rosberry/general/archive/0.2.tar.gz"
     version "0.2"
-    sha256 "09bb0f8c6c1aa09df6842fac44fa20b22c0f1676148512a255ccee48775924b9"
+    sha256 "5e295825f6c2f2651981f569e9d0da7b7d6da82e56d781dc1f5251edf9b58952"
 
     def install
-      system "make", "general"
-      bin.install 'general'
+      system "swift", "build",
+             "--configuration", "release",
+             "--disable-sandbox"
+      bin.install '.build/release/general'
+
       bash_completion.install "Scripts/completions/general" => "general"
       zsh_completion.install  "Scripts/completions/_general" => "_general"
       fish_completion.install "Scripts/completions/general.fish" => "general.fish"
