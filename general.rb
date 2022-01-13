1179666afedbb7be845b7498486e20f745c0ad78
Tublad <tublad92@gmail.com>
2022-01-13 14:39:09 +0600

    update version tag

--- general.rb
+++ general.rb
@@ -1,7 +1,7 @@
 class General < Formula
     desc "Code generation for your modules"
     homepage "https://github.com/rosberry/general"
-    url "https://github.com/rosberry/general/archive/0.3.tar.gz"
+    url "https://github.com/rosberry/general/archive/refs/tags/0.5.tar.gz"
     version "0.5"
     sha256 "0ccc0449f641d7acb83d2f83719fdc993933c0b096abf0893d55539c32a13741"
 
